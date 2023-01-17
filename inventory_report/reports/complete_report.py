4dc5c666e2bb4216352e524464a634e380c08c7e
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2023-01-17 20:41:06 -0300

    Feat: add complete_report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,25 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    def products_by_company(report_list):
+        report_text = ""
+        companies_list = Counter(
+            product["nome_da_empresa"] for product in report_list
+        )
+
+        for company, amount in companies_list.items():
+            report_text += f"- {company}: {amount}\n"
+        return report_text
+
+    @staticmethod
+    def generate(report_list: list):
+
+        simple_report = SimpleReport.generate(report_list)
+
+        return (
+            f"{simple_report}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{CompleteReport.products_by_company(report_list)}"
+        )
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -4,20 +4,20 @@ from datetime import datetime
 
 class SimpleReport:
     @staticmethod
-    def generate(reportList: list):
+    def generate(report_list: list):
 
         today = datetime.now().strftime('%Y-%m-%d')
 
         oldest_date = min(
-            [product["data_de_fabricacao"] for product in reportList]
+            [product["data_de_fabricacao"] for product in report_list]
         )
 
         closest_date = min([product["data_de_validade"]
-                            for product in reportList
+                            for product in report_list
                             if product["data_de_validade"] > today])
 
         bigger_company, _ = Counter([product["nome_da_empresa"]
-                                    for product in reportList]
+                                    for product in report_list]
                                     ).most_common()[0]
 
         return (
