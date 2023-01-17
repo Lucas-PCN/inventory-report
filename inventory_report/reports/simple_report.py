eeb207fff23c30718992d21fdd16ebdb51c12a1b
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2023-01-17 20:05:04 -0300

    Feat: add simple_report

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,27 @@
+from collections import Counter
+from datetime import datetime
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(reportList: list):
+
+        today = datetime.now().strftime('%Y-%m-%d')
+
+        oldest_date = min(
+            [product["data_de_fabricacao"] for product in reportList]
+        )
+
+        closest_date = min([product["data_de_validade"]
+                            for product in reportList
+                            if product["data_de_validade"] > today])
+
+        bigger_company, _ = Counter([product["nome_da_empresa"]
+                                    for product in reportList]
+                                    ).most_common()[0]
+
+        return (
+            f"Data de fabricação mais antiga: {oldest_date}\n"
+            f"Data de validade mais próxima: {closest_date}\n"
+            f"Empresa com mais produtos: {bigger_company}"
+        )
