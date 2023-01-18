8e311cc577b347c787103f684be1558144400fcc
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2023-01-18 09:41:25 -0300

    Feat: add test_product_report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,22 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+            "1",
+            "Iphone",
+            "Apple",
+            "10/12/2022",
+            "undefined",
+            "LXS123456",
+            "em local seco e arejado",
+    )
+
+    result = (
+        f"O produto {product.nome_do_produto} "
+        f"fabricado em {product.data_de_fabricacao} "
+        f"por {product.nome_da_empresa} com validade "
+        f"até {product.data_de_validade} "
+        f"precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
+    assert str(product) == result
